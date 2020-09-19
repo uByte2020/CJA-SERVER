c8705a09316e8c7d0a9c89b9b70fdeab9bb49ad8
Zet Almeida <daniel20150154@gmail.com>
2020-09-19 14:25:38 +0100

    Prepare to deploy for the QUA 6

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,7 @@ const createSendToken = (user, statusCode, res) => {
     httpOnly: true
   };
 
-  if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
+  // if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
 
   res.cookie('jwt', token, cookieOption);
 
