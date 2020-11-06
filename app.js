e006da80dc61525a1d393bd4af234e1ef2610a05
Josia <josiassmith005@gmail.com>
2020-11-06 18:47:40 +0100

    Resolução Do Downloads

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
     //   credentials: true
     // }
     {
-      origin: "*",
+      origin: process.env.CLIENTE_SERVER,
       credentials: true,
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       preflightContinue: false,
